1c097a9116bec7f59600c286a92590be2d3e212e
ytmurill <ytmurill@bancolombia.com.co>
2021-04-05 15:16:09 -0500

    casos de prueba restassured

---
+++ restAssuredRickAndMorty/src/main/java/utils/Constants.java
@@ -0,0 +1,18 @@
+package utils;
+
+public class Constants {
+
+    public static String BASE_URL="https://rickandmortyapi.com/api";
+    public static String CODE_STATUS="codeStatus";
+
+    public static String NON_EXISTENT_PAGE_CODE_STATUS="nonExistentPageCodeStatus";
+
+    public static String PAGINATION_INFO="paginationInfo";
+    public static String RESULT_INFO="resultInfo";
+
+    public static String ERROR_RESPONSE="errorResponse";
+
+    public static String COUNT_CHARACTERS_VALIDATION="countCharactersValidation";
+
+    public static String NAVIGATION_VALIDATION="navigationValidation";
+}
